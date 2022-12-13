3664c012c5f5548e7a2c0b9b3e6e3dfb46c7d299
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-12-13 17:01:44 +0530

    Time: 858 ms (47.68%), Space: 117.1 MB (48.86%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    void dfs(int num, int &ans, vector<int> &vis, vector<vector<int>> &adj, vector<vector<int>> &real)
+    void dfs(int num, int &ans, vector<int> &vis, vector<vector<int>> &adj)
     {
         vis[num] = 1;
         for(auto i: adj[num])
         {
-            if(!vis[i])
+            if(!vis[abs(i)])
             {
-                auto it = find(real[num].begin(), real[num].end(), i);
-                if(it != real[num].end()) ans++;
-                dfs(i, ans, vis, adj, real);
+                if(i > 0) ans++;
+                dfs(abs(i), ans, vis, adj);
             }
         }
     }
     int minReorder(int n, vector<vector<int>>& con) {
-        vector<vector<int>>adj(n), real(n);
+        vector<vector<int>>adj(n);
         int ans = 0;
         vector<int>vis(n);
         for(auto i: con)
         {
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
-            real[i[0]].push_back(i[1]);
+            adj[i[0]].push_back(+i[1]);
+            adj[i[1]].push_back(-i[0]);
         }
-        dfs(0, ans, vis, adj, real);
+        dfs(0, ans, vis, adj);
         return ans;
     }
 };
