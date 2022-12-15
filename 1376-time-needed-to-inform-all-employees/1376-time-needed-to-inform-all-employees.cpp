ee83f90cb341068d5838d6ecaa9cf6636015f991
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-12-15 18:14:10 +0530

    Time: 307 ms (91.24%), Space: 117.8 MB (70.28%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,35 @@
+// bfs se banega
+class Solution {
+public:
+    int numOfMinutes(int n, int headId, vector<int>& manager, vector<int>&info) 
+    {
+        vector<int>adj[n];
+        for(int i=0; i<n; i++)
+        {
+            if(manager[i] != -1)
+            adj[manager[i]].push_back(i);
+        }
+        queue<pair<int, int>>q;
+        q.push({headId, 0});
+        vector<int>vis(n);
+        vis[headId] = 1;
+        int ans = 0;
+        while(!q.empty())
+        {
+            int size = q.size();
+            while(size--)
+            { 
+                int num = q.front().first;
+                int time = q.front().second;
+                q.pop();
+                for(auto i: adj[num])
+                {
+                    int infoTime = time + info[num];
+                    ans = max(ans, infoTime);
+                    if(info[i] != 0) q.push({i, infoTime});
+                }
+            }
+        }
+        return ans;
+    }
+};
