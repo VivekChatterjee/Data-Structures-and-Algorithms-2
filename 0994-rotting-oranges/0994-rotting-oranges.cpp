fade2adc32d0477d273423a167c253740210d6fe
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-12-15 11:39:32 +0530

    Time: 11 ms (70.45%), Space: 13.4 MB (21.42%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,47 +1,49 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) 
-    {
-        
-        vector<int> dir={-1,0,1,0,-1}; 
-        
-        int m=grid.size();
-        int n=grid[0].size();
-        
-        queue<pair<int,int>> q;
+    int orangesRotting(vector<vector<int>> &grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<pair<int,int>> q, q2;
         int fresh=0; 
         for(int i=0;i<m;i++)
+        {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==2)
                 {
-                    q.push({i,j});
+                    q.push({i, j});
+                    q2.push({i,j});
                 }
-                if(grid[i][j]==1)
+                if(grid[i][j] == 1)
                     fresh++;
             }
+        }
         if(fresh == 0) return 0;
         int ans=0;
+        vector<int>row = {-1,0,+1,0};
+        vector<int>col = {0,-1,0,+1};
+
         while(!q.empty())
         {
-            int sz=q.size();
-            while(sz--)
+            int sz = q.size();
+            for(int i = 0; i < sz; i++)
             {
-                pair<int,int> p=q.front();
+                int rr = q.front().first;
+                int cc = q.front().second;
                 q.pop();
-                for(int i=0;i<4;i++)
+                for(int del = 0; del < 4; del++)
                 {
-                    int r=p.first+dir[i];
-                    int c=p.second+dir[i+1];
-                    if(r>=0 && r<m && c>=0 && c<n && grid[r][c]==1)
+                    int r = rr + row[del];
+                    int c = cc + col[del];
+                    if(r>=0 && r<m && c>=0 && c<n && grid[r][c] == 1)
                     {
-                        grid[r][c]=2;
+                        grid[r][c] = 2;
                         q.push({r,c});
                         fresh--;
                     }
                 }
             }
-            ans++; 
+            ans++;
             if(fresh == 0) break;
         }
         return (fresh > 0)? -1 : ans;
