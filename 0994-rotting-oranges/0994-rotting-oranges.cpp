0d85e7c9f6e197a439c1bab43663f6320b3c2f29
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-12-15 12:28:03 +0530

    Time: 11 ms (70.45%), Space: 13.1 MB (82.28%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,7 +3,7 @@ public:
     int orangesRotting(vector<vector<int>> &grid) {
         int m = grid.size();
         int n = grid[0].size();
-        queue<pair<int,int>> q, q2;
+        queue<pair<pair<int,int>, int>> q; // {{index, index}, time}
         int fresh=0; 
         for(int i=0;i<m;i++)
         {
@@ -11,8 +11,7 @@ public:
             {
                 if(grid[i][j]==2)
                 {
-                    q.push({i, j});
-                    q2.push({i,j});
+                    q.push({{i, j}, 0});
                 }
                 if(grid[i][j] == 1)
                     fresh++;
@@ -22,14 +21,14 @@ public:
         int ans=0;
         vector<int>row = {-1,0,+1,0};
         vector<int>col = {0,-1,0,+1};
-
         while(!q.empty())
         {
-            int sz = q.size();
-            for(int i = 0; i < sz; i++)
+            int size = q.size();
+            for(int i = 0; i < size; i++)
             {
-                int rr = q.front().first;
-                int cc = q.front().second;
+                int rr = q.front().first.first;
+                int cc = q.front().first.second;
+                int t = q.front().second;
                 q.pop();
                 for(int del = 0; del < 4; del++)
                 {
@@ -38,13 +37,12 @@ public:
                     if(r>=0 && r<m && c>=0 && c<n && grid[r][c] == 1)
                     {
                         grid[r][c] = 2;
-                        q.push({r,c});
+                        q.push({{r,c}, t+1});
+                        ans = max(ans, t+1);
                         fresh--;
                     }
                 }
             }
-            ans++;
-            if(fresh == 0) break;
         }
         return (fresh > 0)? -1 : ans;
     }
