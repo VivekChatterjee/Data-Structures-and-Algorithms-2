8b2f8b4b959102eaee954d1e1861eb007f4a9bbc
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-12-18 11:02:24 +0530

    Time: 465 ms (31.26%), Space: 98.7 MB (74.20%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -6,10 +6,7 @@ public:
         vector<int> ans(n);
         for(int i=n-1; i>=0; i--)
         {
-            while(!s.empty() && s.top().first <= temp[i])
-            {
-                s.pop();
-            }
+            while(!s.empty() && s.top().first <= temp[i]) s.pop();
             if(s.empty()) ans[i] = 0;
             else ans[i] = s.top().second - i;
             s.push({temp[i], i});
