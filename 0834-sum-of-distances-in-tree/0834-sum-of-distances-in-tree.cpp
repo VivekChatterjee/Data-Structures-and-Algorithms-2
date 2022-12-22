f0f7a1239121a47b0930661d0505bb5fc70f9e7c
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-12-22 17:59:47 +0530

    Time: 290 ms (88.09%), Space: 94.1 MB (40.00%) - LeetHub

---
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> v;
+    vector<int> counter, res;
+        
+    void dfs(int i, int p = -1) {
+        for(auto u : v[i]) {
+            if(u == p) continue;
+            dfs(u, i);
+            counter[i] += counter[u];
+            res[i] += res[u] + counter[u];
+        }
+        counter[i] += 1;
+    }
+
+    void dfs2(int i, int n, int p = -1) {
+        for(auto u : v[i]) {
+            if(u == p) continue;
+            res[u] = res[i] - counter[u] + n - counter[u];
+            dfs2(u, n, i);
+        }
+    }
+    
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        v.resize(n);
+        for(auto i: edges) 
+        {
+            v[i[0]].push_back(i[1]);
+            v[i[1]].push_back(i[0]);
+        }
+        res.resize(n);
+        counter.resize(n);
+        dfs(0);
+        dfs2(0, n);
+        return res;
+    }
+};
