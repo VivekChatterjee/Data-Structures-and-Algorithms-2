5f5ab8e4895771b03f255872223ffc7b7177e44b
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-12-23 01:38:25 +0530

    Time: 19 ms (98.14%), Space: 14 MB (50.27%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,26 +1,64 @@
-class Solution {
-public:
-    void dfs(vector<vector<int>>& c, int j, vector<int>&vis)
+class DisjointSet
+{
+    vector<int>rank, size, parent;
+    public:
+    DisjointSet(int n)
     {
-        int n = c.size();
-        vis[j] = 1;
-        for(int i=0; i<c[j].size();i++)
+        rank.resize(n+1, 0);
+        size.resize(n+1, 1);
+        parent.resize(n+1);
+        for(int i=0; i<=n; i++) parent[i] = i;
+    }
+    int findPar(int node)
+    {
+        if(parent[node] == node) return node;
+        return parent[node] = findPar(parent[node]);
+    }
+    void findUnionByRank(int node1, int node2)
+    {
+        int par1 = findPar(node1);
+        int par2 = findPar(node2);
+        if(par1 == par2) return;
+        if(rank[par2] > rank[par1]) parent[par1] = par2;
+        else if(rank[par1] > rank[par2]) parent[par2] = par1;
+        else
         {
-            if(c[j][i] == 1 && !vis[i]) dfs(c, i, vis);
+            parent[par2] = par1;
+            rank[par1]+=1;
         }
     }
-    int findCircleNum(vector<vector<int>>& c) {
-        int n=c.size();
-        int ans=0;
-        vector<int>vis(n);
+    void findUnionBySize(int node1, int node2)
+    {
+        int par1 = findPar(node1);
+        int par2 = findPar(node2);
+        if(par1 == par2) return;
+        if(size[par1] > size[par2])
+        {
+            parent[par1] = par2;
+            size[par2]+=size[par1];
+        }
+        else
+        {
+            parent[par2] = par1;
+            size[par1]+=size[par2];
+        }
+    }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        DisjointSet d(n);
         for(int i=0; i<n; i++)
         {
-            if(!vis[i])
+            for(int j=0; j<isConnected[i].size(); j++)
             {
-                ans++;
-                dfs(c,i, vis);
+                if(isConnected[i][j] == 1) d.findUnionBySize(i, j);
             }
         }
-        return ans;
+        set<int>ans;
+        for(int i=0; i<n; i++) ans.insert(d.findPar(i));
+        return ans.size();
     }
 };
