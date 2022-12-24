bb5e523d1f19cead8c728c95409f4a48c9b1d9b3
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-12-24 15:53:40 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (60.79%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -3,16 +3,15 @@ class Solution {
 public:
     int K = 1e9+7;
     int numTilings(int n) {
-        vector<ll> v(1001);
-        v[1]=1;
-        v[2]=2;
-        v[3]=5;
-        if(n<=3)
-            return v[n];
+        vector<ll> dp(1001);
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=5;
+        if(n<=3) return dp[n];
         for(int i=4;i<=n;++i){
-            v[i]=2*v[i-1]+v[i-3]; 
-            v[i]%=K;
+            dp[i]=2*dp[i-1]+dp[i-3]; 
+            dp[i]%=K;
         }
-        return v[n];
+        return dp[n];
     }
 };
