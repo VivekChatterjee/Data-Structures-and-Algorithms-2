03e45decf03da6c2fe3f188a71368c5ef5fa4e65
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-01-04 10:42:25 +0530

    Time: 503 ms (42.46%), Space: 104.7 MB (23.86%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        int n = tasks.size();
+        map<int, int>m;
+        for(int i: tasks) m[i]++;
+        int ans = 0;
+        for(auto i: m)
+        {
+            // cout<<i.first<<" : "<<i.second<<" ";
+            // cout<<ans<<" pehle ";
+            if(i.second == 1) return -1;
+            else
+            {
+                int num = i.second;
+                if(num % 3 == 0) ans += num/3;
+                else if(num == 4  || num == 2)
+                {
+                    ans+=(num / 2);
+                }
+                else
+                {
+                    if(num % 3 == 1)
+                    {
+                        int qu = num/3;
+                        ans+=qu-1; // for 3
+                        ans+= (num - (qu-1)*3)/2; // for 2
+                    }
+                    else
+                    {
+                        ans += num/3;
+                        ans += ((num%3)/2);
+                    }
+                }
+            }
+            // cout<<ans<<" last\n";
+        }
+        return ans;        
+    }
+};
