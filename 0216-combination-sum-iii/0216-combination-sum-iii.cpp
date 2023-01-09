0215825c54009fa19d3a770c71e56a7463ce66b5
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-01-09 15:59:02 +0530

    Time: 10 ms (9.62%), Space: 13.7 MB (7.10%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,3 +1,44 @@
+// find all combinations of given numbers
+// class Solution {
+// public:
+//     vector<vector<int>>ans;
+//     void find(int total, int sum, int k, int n, vector<int>v2, unordered_map<int, int>m)
+//     {
+//         if(sum > n) return;
+//         else if(total == k)
+//         {
+//             if(sum == n)
+//             {
+//                 ans.push_back(v2);                
+//                 return;
+//             }
+//             else return;
+//         }
+//         for(int i=1; i<=9; i++)
+//         {
+//             if(!m.count(i))
+//             {
+//                 v2.push_back(i);
+//                 m[i]++;
+//                 find(total+1, sum+i, k, n, v2, m);
+//                 v2.pop_back();
+//                 m[i]--;
+//                 if(m[i] == 0) m.erase(i);
+//             }
+//         }
+//     }
+//     vector<vector<int>> combinationSum3(int k, int n) {
+//         vector<int>v, v2;
+//         unordered_map<int, int>m;
+//         find(0, 0, k, n, v2, m);
+//         return ans;        
+//     }
+// };
+
+
+
+
+
 #define vc vector
 class Solution {
 public:
@@ -14,9 +55,6 @@ public:
             else return;
         }
         else if(sum > n || num > 9) return;
-        
-        // take
-        // cout<<"idhar="<<num<<" sum="<<sum<<endl;
         v.push_back(num);
         find(num+1, total+1, sum+num, k, n, v);
         v.pop_back();
