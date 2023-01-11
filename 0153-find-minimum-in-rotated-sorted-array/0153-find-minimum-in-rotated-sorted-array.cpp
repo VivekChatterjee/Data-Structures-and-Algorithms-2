74678b8274b6392f90e817828ffd72bd9f8fa4fd
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-01-11 11:24:31 +0530

    Time: 10 ms (22.11%), Space: 10.3 MB (27.76%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -8,10 +8,7 @@ public:
         {
             int mid=l+(h-l)/2;
             ans=min(ans, nums[mid]);
-            if(nums[h]<nums[mid])
-            {
-                l=mid+1;
-            }
+            if(nums[h]<nums[mid]) l=mid+1;
             else h=mid-1;
         }
         return ans;
