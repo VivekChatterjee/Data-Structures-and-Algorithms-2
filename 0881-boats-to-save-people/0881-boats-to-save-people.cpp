617e53effa18e67c6a8b119b46f74c9a13389f4e
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-01-14 00:46:55 +0530

    Time: 86 ms (88.82%), Space: 42 MB (11.28%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -4,24 +4,23 @@ public:
         int n=people.size();
         sort(people.begin(), people.end());
         int s=0, e=n-1,ans=0;
-        while(people[e] == limit)
-        {
-            ans++;
-            e--;
-        }
+        // while(people[e] == limit)
+        // {
+        //     ans++;
+        //     e--;
+        // }
         while(s<=e)
         {
             if(people[s]+people[e] <= limit)
             {
-                ans++;
                 s++;
                 e--;
             }
-            else if(people[s]+people[e] > limit)
+            else
             {
-                ans++;
                 e--;
             }
+            ans++;
         }
         return ans;
     }
