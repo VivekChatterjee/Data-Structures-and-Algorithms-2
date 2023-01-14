5bae49b5f626547672e232911cfcc5329fa74b42
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-01-14 11:35:25 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (29.36%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -8,29 +8,19 @@ public:
         size.resize(n + 1, 1);
         parent.resize(n + 1);
         for (int i = 0; i <= n; i++)
-            parent[i] = i;
+            parent[i] = -1;
     }
     int findPar(int node)
     {
-        if (parent[node] == node)
-            return node;
+        if(parent[node] == -1) return node;
         return parent[node] = findPar(parent[node]);
     }
     void unionRank(int node1, int node2)
     {
         int par1 = findPar(node1);
         int par2 = findPar(node2);
-        if (par1 == par2)
-            return;
-        if (rank[par2] > rank[par1])
-            parent[par1] = par2;
-        else if (rank[par1] > rank[par2])
-            parent[par2] = par1;
-        else
-        {
-            parent[par2] = par1;
-            rank[par1] += 1;
-        }
+        if(par1 != par2)
+        parent[max(par1, par2)]=min(par1, par2);
     }
     void unionSize(int node1, int node2)
     {
@@ -60,26 +50,12 @@ public:
         {
             ds.unionRank((s1[i]-'a') , (s2[i]-'a'));
         }
-        unordered_map<int, set<int>>m;
-        for(int i=0; i<n; i++)
-        {
-            int a=s1[i]-'a';
-            int b=s2[i]-'a';
-            int parA=ds.findPar(a);
-            int parB=ds.findPar(b);
-            m[parA].insert(a);
-            m[parA].insert(parA);
-            m[parB].insert(b);
-            m[parB].insert(parB);
-        }
         string s="";
         for(int i=0; i<base.size(); i++)
         {
             int a=base[i]-'a';
             int parA=ds.findPar(a);
-            if(!m.count(parA)) s+=base[i];
-            else
-            s+=(char)(*(m[parA]).begin() + 'a');
+            s+=(parA + 'a');
         }
         return s;
     }
