4adf0075a101512936269af1f3747f13910e602a
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-01-16 23:51:30 +0530

    Time: 60 ms (83.78%), Space: 23.3 MB (53.44%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -3,14 +3,12 @@ public:
     vector<string> findRepeatedDnaSequences(string s) {
         int i=0, j=0, n=s.size();
         string st="";
-        set<string>ss;
         unordered_map<string, int>m;
         while(j<n)
         {
             st+=s[j];
             if(j-i+1==10)
             {
-                ss.insert(st);
                 m[st]++;
                 st.erase(st.begin());
                 i++;
@@ -18,7 +16,7 @@ public:
             j++;
         }
         vector<string>ans;
-        for(auto i: ss) if(m[i] > 1) ans.push_back(i);
+        for(auto i: m) if(i.second > 1) ans.push_back(i.first);
         return ans;
     }
 };
