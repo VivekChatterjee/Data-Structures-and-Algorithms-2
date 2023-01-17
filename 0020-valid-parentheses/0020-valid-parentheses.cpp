bd8968de73bd7a314883eb1c13d240fb0dd615d0
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-01-17 11:10:33 +0530

    Time: 4 ms (36.70%), Space: 6.4 MB (53.04%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        int n=s.size();
+        for(int i=0; i<n; i++)
+        {
+            if(s[i] == ')')
+            {
+                if(st.empty() || st.top() != '(') return 0;
+                st.pop();
+            }
+            else if(s[i] == '}')
+            {
+                if(st.empty() || st.top() != '{') return 0;
+                st.pop();
+            }
+            else if(s[i] == ']')
+            {
+                if(st.empty() || st.top() != '[') return 0;
+                st.pop();
+            }
+            else st.push(s[i]);
+        }
+        return st.empty();
+    }
+};
