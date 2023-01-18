52340973aebaa0e7664bfeac8f198bba9940cda1
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-01-18 13:58:39 +0530

    Time: 4 ms (83.44%), Space: 8.3 MB (85.53%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        int n=deck.size();
+        bool flag=0; // can take when 0, can't take when 1
+        vector<int>ans(n, -1);
+        sort(deck.begin(), deck.end());
+        int i=0, j=0;
+        // j at ans vector, i at deck vector, j should always be moved
+        int total=n;
+        while(1)
+        {
+            if(total == 0) break;
+            if(ans[j] == -1)
+            {
+                if(!flag)
+                {
+                    ans[j]=deck[i];
+                    i++;
+                    total--;
+                }
+                flag=!flag;
+            }
+            j=(j+1)%n;
+        }
+        return ans;
+    }
+};
