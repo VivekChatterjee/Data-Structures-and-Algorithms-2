47c2c3e0e06657d56f52e5aefe249442469f327d
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-01-27 15:42:52 +0530

    Time: 202 ms (41.00%), Space: 65.7 MB (18.75%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>> g; // to store the graph
-    unordered_map<int, bool> visited; // to stop exploring same nodes again and again.
-	
-    void createGraph(vector<vector<int>>& edges) {
-      for (auto e: edges) {
-        g[e[0]].push_back(e[1]); // adjecency list representation
-		g[e[1]].push_back(e[0]); // adjecency list representation
-      }
+    int time(int i, vector<int>adj[], vector<bool>&hasApple, vector<int>&vis)
+    {
+        vis[i]=1;
+        int ct=0;
+        for(auto node: adj[i])
+        {
+            if(!vis[node])
+            {
+                ct += time(node, adj, hasApple, vis);
+            }
+        }
+        if(i == 0)
+        {
+            return ct;
+        }
+        else
+        {
+            if(hasApple[i])
+            {
+                if(ct > 0) return ct+2;
+                else return 2;
+            }
+            else 
+            {
+                if(ct > 0) return ct+2;
+                else return 0;
+            }
+        }
     }
-  
-    int dfs(int node, int myCost, vector<bool>& hasApple) {
-	  if (visited[node]) {
-		  return 0;
-	  }
-	  visited[node] = true;
-	  
-      int childrenCost = 0; // cost of traversing all children. 
-      for (auto x: g[node]) { 
-        childrenCost += dfs(x, 2, hasApple);  // check recursively for all apples.
-      }
-
-      if (childrenCost == 0 && hasApple[node] == false) {
-	  // If no child has apples, then we won't traverse the subtree, so cost will be zero.
-	  // similarly, if current node also does not have the apple, we won't traverse this branch at all, so cost will be zero.
-        return 0;
-      }
-	  
-	  // Children has at least one apple or the current node has an apple, so add those costs.
-      return (childrenCost + myCost);
-    }
-  
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
-      createGraph(edges); // construct the graph first.
-      return dfs(0, 0, hasApple); // cost of reaching the root is 0. For all others, its 2.
+        vector<int>adj[n], vis(n);
+        for(auto i: edges)
+        {
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+        }
+        return time(0, adj, hasApple, vis);
     }
 };
