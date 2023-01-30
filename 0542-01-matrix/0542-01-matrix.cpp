4d56dabc875afe66f7185804fe8db89708a7bf74
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-01-30 12:19:50 +0530

    Time: 93 ms (66.64%), Space: 31.6 MB (54.09%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
-        int m = grid.size();
-	    int n = grid[0].size();
-	    vector<vector<int>>ans(m, vector<int>(n)), vis(m, vector<int>(n));
-	    queue<pair<pair<int,int>, int>>q;
-	    for(int i=0; i<m; i++)
-	    {
-	        for(int j=0; j<n; j++)
-	        {
-	            if(grid[i][j] == 0)
-	            {
-	                vis[i][j] = 1;
-	                q.push({{i, j}, 0});
-	            }
-	        }
-	    }
-	    vector<int>row = {-1, 0, 1, 0};
-	    vector<int>col = {0, -1, 0, 1};
-	    while(!q.empty())
-	    {
-	        int size = q.size();
-	        while(size--)
-	        {
-    	       int r= q.front().first.first;
-    	       int c = q.front().first.second;
-    	       int dis = q.front().second;
-    	       q.pop();
-    	       ans[r][c] = dis;
-    	       for(int i=0; i<4; i++)
-    	       {
-    	            int rr = r+row[i];
-    	            int cc = c+col[i];
-    	            if(rr>=0 && rr<m && cc>=0 && cc<n && !vis[rr][cc])
-    	            {
-    	                vis[rr][cc] = 1;
-    	                q.push({{rr, cc}, dis+1});
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        queue<pair<int, pair<int, int>>>q;
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>>ans(m, vector<int>(n, -1));
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(mat[i][j] == 0) 
+                {
+                    q.push({0, {i, j}});
+                    ans[i][j]=0;
+                }
+            }
+        }
+        vector<int>row={-1, 0, 1, 0}, col={0, -1, 0, 1};
+        while(!q.empty())
+        {
+            int dis=q.front().first;
+            int ii=q.front().second.first;
+            int jj=q.front().second.second;
+            q.pop();
+            for(int i=0; i<4; i++)
+            {
+                int rr=row[i]+ii;
+                int cc=col[i]+jj;
+                if(rr>=0 && rr<m && cc>=0 && cc<n)
+                {
+                    if(ans[rr][cc] == -1)
+                    {
+                        ans[rr][cc]=dis+1;
+                        q.push({dis+1, {rr, cc}});
                     }
                 }
-	        }
-	    }
-	    return ans;
-	}
+            }
+        }
+        return ans;
+    }
 };
