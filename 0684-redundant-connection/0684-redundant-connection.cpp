b2ad75968c166e967ad13f6ed391cefc2595b71d
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-02-01 23:06:28 +0530

    Time: 13 ms (42.66%), Space: 9.1 MB (48.73%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,68 +1,23 @@
-class DisjointSet
-{
-public:
-    vector<int> rank, size, parent;
-    DisjointSet(int n)
-    {
-        rank.resize(n + 1, 0);
-        size.resize(n + 1, 1);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++)
-            parent[i] = i;
-    }
-    int findPar(int node)
-    {
-        if (parent[node] == node)
-            return node;
-        return parent[node] = findPar(parent[node]);
-    }
-    void unionRank(int node1, int node2)
-    {
-        int par1 = findPar(node1);
-        int par2 = findPar(node2);
-        if (par1 == par2)
-            return;
-        if (rank[par2] > rank[par1])
-            parent[par1] = par2;
-        else if (rank[par1] > rank[par2])
-            parent[par2] = par1;
-        else
-        {
-            parent[par2] = par1;
-            rank[par1] += 1;
-        }
-    }
-    void unionSize(int node1, int node2)
-    {
-        int par1 = findPar(node1);
-        int par2 = findPar(node2);
-        if (par1 == par2)
-            return;
-        if (size[par1] > size[par2])
-        {
-            parent[par1] = par2;
-            size[par2] += size[par1];
-        }
-        else
-        {
-            parent[par2] = par1;
-            size[par1] += size[par2];
-        }
-    }
-};
 class Solution {
 public:
+    vector<int>par, size;
+    int findPar(int a)
+    {
+        if(par[a] == a) return a;
+        return par[a] = findPar(par[a]);
+    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        DisjointSet d(n);
-        vector<pair<int, int>>ans;
-        for(int i=0; i<n; i++)
+        int n=edges.size();
+        par.resize(n+1), size.resize(n+1);
+        for(int i=0; i<=n; i++)
+            par[i]=i;
+        for(auto it: edges)
         {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            if(d.findPar(u) != d.findPar(v)) d.unionRank(u, v);
-            else return {u, v};
+            int parA=findPar(it[0]);
+            int parB=findPar(it[1]);
+            if(parA == parB) return {it[0], it[1]};
+            par[parA]=parB;
         }
-        return {};
+        return {-1, -1};
     }
 };
