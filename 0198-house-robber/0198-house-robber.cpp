26c718181184bd53bd8d1546f4a81b620d18cf49
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-02-06 16:08:47 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (6.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,11 +4,10 @@ public:
         int n=nums.size();
         vector<int>dp(n);
         dp[0]=nums[0];
-        if(n == 1) return dp[0];
-        dp[1]=max(dp[0], nums[1]);
-        for(int i=2; i<n; i++)
+        for(int i=1; i<n; i++)
         {
-            dp[i]=max(dp[i-2]+nums[i], dp[i-1]);
+            if(i == 1) dp[i]=max(dp[i-1], nums[i]);
+            else dp[i]=max(dp[i-2]+nums[i], dp[i-1]);
         }
         return dp[n-1];
     }
