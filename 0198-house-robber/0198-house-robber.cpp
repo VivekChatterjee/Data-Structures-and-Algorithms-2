fb2c314e92e4b3e61852207635fa4a306529336d
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-02-06 16:07:47 +0530

    Time: 5 ms (25.05%), Space: 7.9 MB (6.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    int robIt(int i, vector<int>&nums, vector<int>&dp)
-    {
-        int n=nums.size();
-        if(i>=n) return 0;
-        if(dp[i] != -1) return dp[i];
-        int take=0, dont=0;
-        dont=0+robIt(i+1, nums, dp);
-        take=nums[i]+robIt(i+2, nums, dp);
-        return dp[i]=max(take, dont);
-    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n, -1);
-        return robIt(0, nums, dp);
+        vector<int>dp(n);
+        dp[0]=nums[0];
+        if(n == 1) return dp[0];
+        dp[1]=max(dp[0], nums[1]);
+        for(int i=2; i<n; i++)
+        {
+            dp[i]=max(dp[i-2]+nums[i], dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
