894c9aea438653d885c0ed397d7222e5ede8fe8b
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-02-09 11:36:38 +0530

    Time: 111 ms (37.30%), Space: 21 MB (54.09%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,56 +1,32 @@
 class Solution {
 public:
-    int maxAreaHistogram(int i, vector<vector<int>>& matrix)
-    {
-        int n=matrix[0].size();
-        stack<int>s,st;
-        vector<int>left(n), right(n);
-        for(int j=n-1; j>=0; j--)
-        {
-            while(!s.empty() and matrix[i][s.top()] >= matrix[i][j]) s.pop();
-            right[j]=(s.empty())? n : s.top();
-            s.push(j);
-        }
-        for(int j=0; j<n; j++)
-        {
-            while(!st.empty() and matrix[i][st.top()] >= matrix[i][j]) st.pop();
-            left[j]=(st.empty())? -1 : st.top();
-            st.push(j);
-        }
-        int ans=INT_MIN;
-        for(int j=0; j<n; j++)
-        {
-            int width = (right[j] - left[j] - 1);
-            int height = matrix[i][j];
-            int side = min(width, height);
-            ans = max(ans, side*side);
-        }
-        return ans;
-    }
     int maximalSquare(vector<vector<char>>& matrix) {
-        int r=matrix.size(), c=matrix[0].size(), ans=INT_MIN;
-        vector<vector<int>>vec(r, vector<int>(c));
-        for(int i=0; i<r; i++)
+        int m=matrix.size(), n=matrix[0].size();
+        vector<vector<int>>dp(m+1, vector<int>(n));
+        bool pos=0;
+        for(int i=0; i<m; i++)
         {
-            for(int j=0; j<c; j++)
+            dp[i][0]=(matrix[i][0]=='1');
+            if(matrix[i][0]=='1') pos=1;            
+        }
+        for(int j=0; j<n; j++)
+        {
+            dp[0][j]=(matrix[0][j]=='1');
+            if(matrix[0][j]=='1') pos=1;   
+        }
+        int ans=0;
+        if(pos) ans=1;
+        for(int i=1; i<m; i++)
+        {
+            for(int j=1; j<n; j++)
             {
-                if(i==0)
+                if(matrix[i][j]=='1')
                 {
-                    vec[i][j] = matrix[i][j] - '0';
-                }
-                else if(matrix[i][j] != '0')
-                {
-                    if(matrix[i-1][j] != '0')
-                        vec[i][j] = 1 + (vec[i-1][j]);
-                    else
-                        vec[i][j] = matrix[i][j] - '0';
+                    dp[i][j]=min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]})+1;
+                    ans=max(ans, dp[i][j]);
                 }
             }
         }
-        for(int i=0; i<r; i++)
-        {
-            ans=max(ans, maxAreaHistogram(i, vec));
-        }
-        return ans;
+        return ans*ans;
     }
 };
