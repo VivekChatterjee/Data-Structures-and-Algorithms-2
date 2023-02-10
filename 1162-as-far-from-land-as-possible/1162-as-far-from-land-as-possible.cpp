a7a0f5dcdb37ec438da6c7e1c26e0437dcdba931
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-02-10 10:49:21 +0530

    Time: 74 ms (63.78%), Space: 21.3 MB (55.25%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,43 +1,45 @@
-#define pii pair<int, int>
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        int n = grid.size();
-        queue<pii>q; // {distance, {row, col}}
+        int n=grid[0].size();
+        queue<pair<int, int>>q;
+        vector<vector<int>>vis(n, vector<int>(n));
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<n; j++)
             {
-                if(grid[i][j] == 1)
+                if(grid[i][j]==1)
                 {
-                    q.push({i-1, j});
-                    q.push({i+1, j});
-                    q.push({i, j-1});
-                    q.push({i, j+1});
+                    q.push({i, j});
+                    vis[i][j]=1;
                 }
             }
         }
-        int ans = 0;
-        vector<int>row = {0, -1, 0, 1}, col = {-1, 0, 1, 0};
+        int ans=0;
+        vector<int>row={-1,0,1,0}, col={0,-1,0,1};
         while(!q.empty())
         {
-            int size = q.size();
-            ans++;
-            for(int i=0; i<size; i++)
+            int sz=q.size();
+            for(int i=0; i<sz; i++)
             {
-                int r = q.front().first;
-                int c = q.front().second;
+                int rr=q.front().first, cc=q.front().second;
                 q.pop();
-                if(r>=0 && r<n && c>=0 && c<n && grid[r][c] == 0)
+                for(int k=0; k<4; k++)
                 {
-                    grid[r][c] = ans;
-                    q.push({r-1, c});
-                    q.push({r+1, c});
-                    q.push({r, c-1});
-                    q.push({r, c+1});
+                    int r=row[k]+rr, c=col[k]+cc;
+                    if(r>=0 and r<n and c>=0 and c<n)
+                    {
+                        if(!vis[r][c])
+                        {
+                            vis[r][c]=1;
+                            q.push({r, c});
+                        }
+                    }
                 }
             }
+            if(q.empty()) break;
+            ans++;
         }
-        return (ans == 1)? -1 : ans - 1 ;
+        return (ans==0)? -1 : ans;
     }
 };
