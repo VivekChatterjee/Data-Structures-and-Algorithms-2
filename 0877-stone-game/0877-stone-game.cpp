f93657b4b2b80141dee26d9c7a8d91044bd44a20
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-02-17 20:36:12 +0530

    Time: 6 ms (47.57%), Space: 7.5 MB (69.01%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n=piles.size();
+        int odd=0, even=0;
+        for(int i=0; i<n; i++)
+        {
+            if(i%2) odd+=piles[i];
+            else even+=piles[i];
+        }
+        return (odd>even || even>odd);
     }
 };
