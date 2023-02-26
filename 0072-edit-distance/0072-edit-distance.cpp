8007e9c8ec6934504d8469699183556d9423a18f
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-02-26 12:53:46 +0530

    Time: 3 ms (99.22%), Space: 9.1 MB (23.71%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    int minDistance(string s, string t) {
-        int n=s.size(), n2=t.size();
-        vector<vector<int>>dp(n+1, vector<int>(n2+1));
-        for(int i=1; i<=n; i++) dp[i][0]=i;
-        for(int j=1; j<=n2; j++) dp[0][j]=j;
-        for(int i=1; i<=n; i++)
+    int distance(int i, int j, string &s, string &t, vector<vector<int>>&dp)
+    {
+        int n=s.size(), nn=t.size();
+        if(i==n && j==nn) return 0;
+        if(i==n) return nn-j;
+        if(j==nn) return n-i;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int del=1e8, ins=1e8, rep=1e8;
+        int ans=1e8;
+        if(s[i]==t[j]) ans=0+distance(i+1, j+1, s, t, dp);
+        else
         {
-            for(int j=1; j<=n2; j++)
-            {
-                if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1];
-                else
-                {
-                    // del, rep, ins
-                    int mini=1+min({dp[i-1][j], dp[i-1][j-1], dp[i][j-1]});
-                    dp[i][j]=mini;
-                }
-            }
+            del=1+distance(i+1, j, s, t, dp);
+            ins=1+distance(i, j+1, s, t, dp);
+            rep=1+distance(i+1, j+1, s, t, dp);
         }
-        return dp[n][n2];
+        ans=min({ans, del, ins, rep});
+        return dp[i][j]=ans;
+    }
+    int minDistance(string s, string t) {
+        int n=s.size(), nn=t.size();
+        vector<vector<int>>dp(n, vector<int>(nn, -1));
+        return distance(0, 0, s, t, dp);
     }
 };
