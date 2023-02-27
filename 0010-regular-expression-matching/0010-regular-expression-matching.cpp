b7e59a9a36ed894a5ef944cc0b1ce70282b78b60
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-02-27 23:33:54 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (34.00%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    bool match(int i, int j, string &s, string &p, vector<vector<int>>&dp)
+    bool match(int i, int j, string& s, string& p, vector<vector<int>>&dp)
     {
-        int n=s.size(), np=p.size();
-        if(i==-1 and j==-1) return 1; //both ends
-        else if(i==-1) return (p[j]=='*' and match(i, j-2, s, p, dp));
-        else if(j==-1) return 0; //only pattern ends
-        if(dp[i][j]!=-1) return dp[i][j];
-        //single character matches
-        if(p[j]=='.' or p[j]==s[i]) return dp[i][j]=match(i-1,j-1, s, p, dp);
-        if(p[j]=='*')
+        if(i<0 && j<0) return 1;
+        if(j<0) return 0;
+        if(i<0)
         {
-            bool one=  match(i, j-2, s, p, dp);
-            bool two=match(i, j-1, s, p, dp);
-            bool three= ((p[j-1]=='.' or s[i]==p[j-1]) and match(i-1, j, s, p, dp));
-            return dp[i][j]=(one|two|three);
+            return (p[j]=='*' && match(i, j-2, s, p, dp));
         }
-        return 0;
-    }
-    bool isMatch(string s, string p) 
-    {
+        if(dp[i][j]!=-1) return dp[i][j];
+        bool ans=0;
+        if(p[j]=='.' || s[i]==p[j])
+            return dp[i][j]=match(i-1, j-1, s, p, dp);
+        else if(p[j]=='*')
+        {
+            bool one=0, two=0;
+            // take no char
+            one=match(i, j-2, s, p, dp);
+            // take single char + multi char
+            two=((p[j-1]=='.' || s[i]==p[j-1]) && match(i-1, j, s, p, dp));   
+            ans=one||two;
+        }
+        return dp[i][j]=ans;
+    }        
+    bool isMatch(string s, string p) {
         int n=s.size(), np=p.size();
         vector<vector<int>>dp(n+1, vector<int>(np+1, -1));
         return match(n-1, np-1, s, p, dp);
