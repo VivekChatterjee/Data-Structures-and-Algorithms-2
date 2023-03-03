59256e90a7bd098c68bd0a746c574bb1b3067379
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-03-04 00:48:27 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (69.99%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int strStr(string s, string t) {
         if(s.size()<t.size()) return -1;
-        return (s.find(t)==s.size())? -1 : s.find(t);
+        int ind=s.find(t);
+        if(ind==s.size()) return -1;
+        else return ind;
     }
 };
