0b51db2987d2a12e3619578a4a4b06a6bd36f771
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-03-04 01:16:47 +0530

    Time: 40 ms (93.91%), Space: 17 MB (53.94%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,75 +1,50 @@
-class DisjointSet
-{
-public:
-    vector<int> rank, size, parent;
-    DisjointSet(int n)
-    {
-        rank.resize(n + 1, 0);
-        size.resize(n + 1, 1);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++)
-            parent[i] = i;
-    }
-    int findPar(int node)
-    {
-        if (parent[node] == node)
-            return node;
-        return parent[node] = findPar(parent[node]);
-    }
-    void unionRank(int node1, int node2)
-    {
-        int par1 = findPar(node1);
-        int par2 = findPar(node2);
-        if (par1 == par2)
-            return;
-        if (rank[par2] > rank[par1])
-            parent[par1] = par2;
-        else if (rank[par1] > rank[par2])
-            parent[par2] = par1;
-        else
-        {
-            parent[par2] = par1;
-            rank[par1] += 1;
-        }
-    }
-    void unionSize(int node1, int node2)
-    {
-        int par1 = findPar(node1);
-        int par2 = findPar(node2);
-        if (par1 == par2)
-            return;
-        if (size[par1] > size[par2])
-        {
-            parent[par1] = par2;
-            size[par2] += size[par1];
-        }
-        else
-        {
-            parent[par2] = par1;
-            size[par1] += size[par2];
-        }
-    }
-};
 class Solution {
 public:
+    vector<int>par, sz;
+    int findPar(int node)
+    {
+        if(par[node]==node) return node;
+        return par[node]=findPar(par[node]);
+    }
+    void unionSize(int a, int b)
+    {
+        int parA=findPar(a);
+        int parB=findPar(b);
+        if(parA==parB) return;
+        if(sz[parA]>=sz[parB])
+        {
+            par[parB]=parA;
+            sz[parA]+=sz[parB];
+        }
+        else
+        {
+            par[parA]=parB;
+            sz[parB]+=sz[parA];
+        }
+    }
     int removeStones(vector<vector<int>>& stones) {
-        int maxRow = 0, maxCol = 0;
+        int n=stones.size(), maxr=0, maxc=0;
+        for(int i=0; i<n; i++)
+        {
+            maxr=max(maxr, stones[i][0]);
+            maxc=max(maxc, stones[i][1]);
+        }
+        par.resize(maxr+maxc+2);
+        sz.resize(maxr+maxc+2, 1);
+        for(int i=0; i<par.size(); i++)
+        {
+            par[i]=i;
+        }
         for(auto it: stones)
         {
-            maxRow = max(maxRow, it[0]);
-            maxCol = max(maxCol, it[1]);
+            unionSize(it[0], it[1]+maxr+1);
         }
-        int n = stones.size();
-        DisjointSet d(maxRow + maxCol + 1);
-        for(auto it: stones)
+        int ct=0;
+        for(int i=0; i<maxr+maxc+2; i++)
         {
-            d.unionSize(it[0], maxRow + it[1] + 1);
+            int parent=findPar(i);
+            if(parent==i && sz[parent]>1) ct++;
         }
-        int connectedComponents = 0;
-        for(int i=0; i<=maxRow + 1 + maxCol; i++)
-        {
-            if(d.findPar(i) == i && d.size[i] > 1) connectedComponents++;
-        }
-        return (n - connectedComponents);
+        return n-ct;
     }
 };
