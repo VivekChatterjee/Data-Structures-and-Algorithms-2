47dff73728ea3eed661affdcc5a2294b898d63a1
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-03-22 23:52:08 +0530

    Time: 507 ms (63.85%), Space: 135.2 MB (48.93%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,50 +1,31 @@
 class Solution {
 public:
-    vector<int>par, size, dis;
-    int findPar(int a)
-    {
-        if(par[a]==a) return a;
-        return par[a]=findPar(par[a]);
-    }
-    void unionSize(int a, int b, int path)
-    {
-        int parA=findPar(a);
-        int parB=findPar(b);
-        if(parA==parB)
-        {
-        dis[parA]=min(dis[parA], path);
-        dis[parB]=min(dis[parB], path);
-         return;
-        }
-        else if(size[parA]>size[parB])
-        {
-            dis[parA]=min(dis[parA], path);
-            par[parB]=parA;
-            size[parA]+=size[parB];
-        }
-        else
-        {
-            dis[parB]=min(dis[parB], path);
-            par[parA]=parB;
-            size[parB]+=size[parA];
-        }
-    }
     int minScore(int n, vector<vector<int>>& roads) {
-        par.resize(n+1);
-        size.resize(n+1, 1);
-        dis.resize(n+1, 1e6);
-        for(int i=1; i<=n; i++)
+        vector<pair<int, int>>adj[n+1];
+        for(auto it: roads)
         {
-            par[i]=i;
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
         }
-        for(auto i: roads)
+        vector<int>vis(n+1);
+        int ans=1e8;
+        queue<int>q;
+        q.push(1);
+        vis[1]=1;
+        while(!q.empty())
         {
-            unionSize(i[0], i[1], i[2]);
+            int node=q.front();
+            q.pop();
+            for(auto &[nbr, dis]: adj[node])
+            {
+                ans=min(ans, dis);
+                if(!vis[nbr])
+                {
+                    vis[nbr]=1;
+                    q.push(nbr);
+                }
+            }
         }
-        for(auto i: roads)
-        {
-            unionSize(i[0], i[1], i[2]);
-        }
-        return dis[findPar(1)];
+        return ans;
     }
 };
