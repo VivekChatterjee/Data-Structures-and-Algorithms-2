88c5d29931679a30154faa1e4e6bef8b1ba35d1e
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-03-24 08:01:30 +0530

    Time: 396 ms (68.87%), Space: 106.3 MB (79.59%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-    void dfs(int num, int &ans, vector<int> &vis, vector<vector<int>> &adj)
-    {
-        vis[num] = 1;
-        for(auto i: adj[num])
+    int minReorder(int n, vector<vector<int>>& c) {
+        vector<pair<int, int>>adj[n];
+        // u -> v
+        // +1 for direction towards 0 (v -> u)
+        // -1 for direction away from 0 (u -> v)
+        for(auto i: c)
         {
-            if(!vis[abs(i)])
+            adj[i[0]].push_back({i[1], -1});
+            adj[i[1]].push_back({i[0], +1});
+        }
+        vector<int>vis(n);
+        queue<int>q;
+        int ans=0;
+        q.push(0);
+        vis[0]=1;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            for(auto &[nbr, dir]: adj[node])
             {
-                if(i > 0) ans++;
-                dfs(abs(i), ans, vis, adj);
+                if(!vis[nbr])
+                {
+                    if(dir<0)
+                    {
+                        ans+=-dir;
+                    }
+                    vis[nbr]=1;
+                    q.push(nbr);
+                }
             }
         }
-    }
-    int minReorder(int n, vector<vector<int>>& con) {
-        vector<vector<int>>adj(n);
-        int ans = 0;
-        vector<int>vis(n);
-        for(auto i: con)
-        {
-            adj[i[0]].push_back(+i[1]);
-            adj[i[1]].push_back(-i[0]);
-        }
-        dfs(0, ans, vis, adj);
         return ans;
     }
 };
