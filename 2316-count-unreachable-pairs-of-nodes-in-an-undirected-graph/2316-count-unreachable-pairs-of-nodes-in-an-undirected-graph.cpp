dca59c26d813764f0a1aa2f02d6ba984f6609508
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-03-25 14:31:50 +0530

    Time: 658 ms (55.95%), Space: 199.1 MB (44.67%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,7 +1,7 @@
 #define ll long long
 class Solution {
 public:
-    void dfs(int &cur, int i, vector<int>adj[], vector<int>&vis)
+    void dfs(ll &cur, int i, vector<int>adj[], vector<int>&vis)
     {
         vis[i]=1;
         cur++;
@@ -14,7 +14,6 @@ public:
         }
     }
     long long countPairs(int n, vector<vector<int>>& edges) {
-        vector<ll>nodes, pre;
         vector<int>adj[n];
         for(auto i: edges)
         {
@@ -22,26 +21,15 @@ public:
             adj[i[1]].push_back(i[0]);
         }
         vector<int>vis(n);
+        ll ans=n*1LL*((n-1)*1LL)/2;
         for(int i=0; i<n; i++)
         {
             if(!vis[i])
             {
-                int cur=0;
+                ll cur=0;
                 dfs(cur, i, adj, vis);
-                nodes.push_back(cur*1LL);
+                ans-=(cur*(cur-1)/2);
             }
-        }
-        n=nodes.size();
-        pre.push_back(nodes[0]);
-        for(int i=1; i<n; i++)
-        {
-            pre.push_back(pre[i-1]+nodes[i]);
-        }
-        ll last=pre[n-1];
-        ll ans=0;
-        for(int i=0; i<n-1; i++)
-        {
-            ans+=(last-pre[i])*nodes[i];
         }
         return ans;
     }
