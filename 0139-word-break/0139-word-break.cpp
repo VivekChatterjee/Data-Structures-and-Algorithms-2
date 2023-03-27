106ca0c75f8fa090a3772c7d7317c348aa84e1f2
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-03-27 23:35:41 +0530

    Time: 24 ms (30.20%), Space: 13.4 MB (34.27%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    bool breakWord(int i, int len, string &s, unordered_map<string, int>&m,             vector<vector<int>>&dp)
+    bool breakWord(int i, string &s, unordered_map<string, int>&m,                       vector<int>&dp)
     {
         int n=s.size();
-        if(i==n)
-        {
-            return n==len;
-        }
-        if(dp[i][len] != -1)
-        {
-         return dp[i][len];
-        }
-        string cur="";
-        bool pos=0;
+        if(i==n) return 1;
+        if(dp[i] != -1) return dp[i];
         for(int k=i; k<n; k++)
         {
-            cur+=s[k];
-            if(m.count(cur))
-            {
-                len+=cur.size();
-                pos|=breakWord(k+1, len, s, m, dp);
-                len-=cur.size();
-            }
-            if(pos) return 1;
+            if(m.count(s.substr(i, k-i+1)) && breakWord(k+1, s, m, dp))
+                return dp[i]=1;
         }
-        return dp[i][len]=pos;
+        return dp[i]=0;
     }
     bool wordBreak(string s, vector<string>& w) {
         int n=s.size();
         unordered_map<string, int>m;
         for(auto &i: w) m[i]++;
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        return breakWord(0, 0, s, m, dp);
+        vector<int>dp(n+1, -1);
+        return breakWord(0, s, m, dp);
     }
 };
