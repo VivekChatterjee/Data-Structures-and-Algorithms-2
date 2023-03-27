761f313ace809113e6eb917fecaf61b8cd0d9f2f
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-03-28 00:11:56 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (95.25%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     vector<string> ans;
-    bool breakWord(int i, string &s, unordered_map<string, int>&m,                       vector<int>&dp, string &a)
+    bool breakWord(int i, string &s, unordered_map<string, int>&m,                       string &a)
     {
         int n=s.size();
-        if(i==n)
-        {
-            return 1;
-        }
-        // if(dp[i] != -1) return dp[i];
+        if(i==n) return 1;
         for(int k=i; k<n; k++)
         {
             string cur=s.substr(i, k-i+1);
@@ -16,7 +12,7 @@ public:
             {
                 string b=a;
                 a=a+cur+" ";
-                if(breakWord(k+1, s, m, dp, a))
+                if(breakWord(k+1, s, m, a))
                 {
                     a.pop_back();
                     ans.push_back(a);
@@ -30,9 +26,8 @@ public:
         int n=s.size();
         unordered_map<string, int>m;
         for(auto &i: w) m[i]++;
-        vector<int>dp(n+1, -1);
         string a="";
-        breakWord(0, s, m, dp, a);
+        breakWord(0, s, m, a);
         return ans;
     }
 };
