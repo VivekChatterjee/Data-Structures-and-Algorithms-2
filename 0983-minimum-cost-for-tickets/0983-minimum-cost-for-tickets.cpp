16ef749cf34d9eb0e0b77747c650b2c29dfb5bd3
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-03-28 17:54:20 +0530

    Time: 15 ms (7.03%), Space: 19.2 MB (6.78%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,53 +1,19 @@
 class Solution {
 public:
-    int cost(int i, vector<int>& days, vector<int>& costs, vector<int>&dp)
+    int cost(int i, vector<int>& days, vector<int>& costs, int canGoTill,  vector<vector<int>>&dp)
     {
         int n=days.size();
-        if(i==n)
-        {
-            return 0;
-        }
-        if(dp[i]!=-1) return dp[i];
-        int one=0, seven=0, month=0;
-        one=costs[0]+cost(i+1, days, costs, dp);
-        int toSeven=-1, toMonth=-1;
-        for(int j=i+1; j<n; j++)
-        {
-            if(days[j]-days[i]>=7)
-            {
-                toSeven=j;
-                break;
-            }
-        }
-        if(toSeven==-1)
-        {
-            seven=costs[1]+cost(n, days, costs, dp);
-        }
-        else
-        {
-            seven=costs[1]+cost(toSeven, days, costs, dp);
-        }
-        for(int j=i+1; j<n; j++)
-        {
-            if(days[j]-days[i]>=30)
-            {
-                toMonth=j;
-                break;
-            }
-        }
-        if(toMonth==-1)
-        {
-            month=costs[2]+cost(n, days, costs, dp);
-        }
-        else
-        {
-            month=costs[2]+cost(toMonth, days, costs, dp);
-        }
-        return dp[i]=min({one, seven, month});
+        if(i==n) return 0;
+        if(dp[i][canGoTill]!=-1) return dp[i][canGoTill];
+        if(days[i]<=canGoTill) return dp[i][canGoTill]=cost(i+1, days, costs, canGoTill, dp);
+        int one=costs[0]+cost(i+1, days, costs, days[i], dp);
+        int seven=costs[1]+cost(i+1, days, costs, days[i]+7-1, dp);
+        int month=costs[2]+cost(i+1, days, costs, days[i]+30-1, dp);
+        return dp[i][canGoTill]=min({one, seven, month});
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n=days.size();
-        vector<int>dp(n, -1);
-        return cost(0, days, costs, dp);
+        vector<vector<int>>dp(n, vector<int>(396, -1));
+        return cost(0, days, costs, 0, dp);
     }
 };
