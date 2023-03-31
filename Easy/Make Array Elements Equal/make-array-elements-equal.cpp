1012c6113a5ed69ce14dc9a0c764ce593c62d092
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-04-01 00:53:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Make Array Elements Equal/make-array-elements-equal.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+#define ll long long
+class Solution {
+  public:
+    long long int minOperations(int n) {
+        ll mid=n/2;
+        return mid*=(n+1)/2;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.minOperations(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
