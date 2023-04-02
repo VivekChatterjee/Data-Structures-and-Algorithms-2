2b26e347e9895b3e91ac87d5df413e340251bfb2
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-04-02 15:15:52 +0530

    Time: 241 ms (45.45%), Space: 110.3 MB (9.09%) - LeetHub

---
+++ 2611-mice-and-cheese/2611-mice-and-cheese.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    static bool cmp(pair<int, int>&a, pair<int, int>&b)
+    {
+        return a.second-a.first < b.second-b.first;
+    }
+    int miceAndCheese(vector<int>& a, vector<int>& b, int k) {
+        vector<pair<int, int>>v;
+        int n=a.size();
+        for(int i=0; i<n; i++)
+        {
+            v.push_back({a[i], b[i]});
+        }
+        sort(v.begin(), v.end(), cmp);
+        int ans=0;
+        for(int i=0; i<n; i++)
+        {
+            if(i<k)
+                ans+=v[i].first;
+            else
+                ans+=v[i].second;
+        }
+        return ans;
+    }
+};
