26acdabee9143493769d66bd7a1b7905f6866163
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-04-05 18:25:59 +0530

    Time: 144 ms (65.39%), Space: 71.4 MB (61.54%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,42 +1,12 @@
 #define ll long long
 class Solution {
 public:
-    bool isValid(int mid, vector<ll>v)
-    {
-        int n=v.size();
-        for(int i=n-1; i>0; i--)
-        {
-            if(v[i]>mid)
-            {
-                ll chances=v[i]-mid;
-                v[i]=mid;
-                v[i-1]+=chances;
-            }
-        }
-        return v[0]<=mid;
-    }
     int minimizeArrayValue(vector<int>& nums) {
-        int n=nums.size();
-        int s=nums[0], e=INT_MIN;
-        int ans=e;
-        vector<ll>v;
-        for(int i: nums)
+        ll n=nums.size(), sum=0, ans=INT_MIN;
+        for(int i=0; i<n; i++)
         {
-            v.push_back(i*1LL);
-            e=max(e, i);
-        }
-        while(s<=e)
-        {
-            int mid=s+(e-s)/2;
-            if(isValid(mid, v))
-            {
-                ans=mid;
-                e=mid-1;
-            }
-            else
-            {
-                s=mid+1;
-            }
+            sum+=nums[i];
+            ans=max(ans, (sum+i)/(i+1));
         }
         return ans;
     }
