d60c98f01559de9ade4f5f4e6a6f844507d9aa20
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-04-06 20:01:58 +0530

    Time: 13 ms (71.49%), Space: 9.4 MB (77.35%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,60 +1,48 @@
 class Solution {
 public:
-    void removeCorners(int i, int j, vector<vector<int>>& grid)
+    void dfs(int i, int j, vector<vector<int>>& grid)
     {
-        grid[i][j] = -1;
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<int>row = {0, -1, 0, +1}, col = {-1, 0, +1, 0};
-        for(int num=0; num<4; num++)
-        {
-            int r = i + row[num];
-            int c = j + col[num];
-            if(r>=0 && r<m && c>=0 && c<n && grid[r][c] == 0) 
-            {
-                removeCorners(r, c, grid);
-            }
-        }
-    }
-    void closed(int i, int j, vector<vector<int>>& grid)
-    {
-        grid[i][j] = 1;
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<int>row = {0, -1, 0, +1}, col = {-1, 0, +1, 0};
-        for(int num=0; num<4; num++)
-        {
-            int r = i + row[num];
-            int c = j + col[num];
-            if(r>=0 && r<m && c>=0 && c<n && grid[r][c] == 0) 
-            {
-                closed(r, c, grid);
-            }
-        }
+        int m=grid.size(), n=grid[0].size();
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==1) return;
+        grid[i][j]=1;
+        dfs(i+1, j, grid);
+        dfs(i-1, j, grid);
+        dfs(i, j+1, grid);
+        dfs(i, j-1, grid);
     }
     int closedIsland(vector<vector<int>>& grid) {
-        // excluding zeroes on the corners they can't be closed island
-        int m = grid.size();
-        int n = grid[0].size();
+        int m=grid.size(), n=grid[0].size();
         for(int i=0; i<m; i++)
         {
-            if(grid[i][0] == 0) removeCorners(i, 0, grid);
-            if(grid[i][n-1] == 0) removeCorners(i, n-1, grid);
+            if(grid[i][0]==0)
+            {
+                dfs(i, 0, grid);
+            }
+            if(grid[i][n-1]==0)
+            {
+                dfs(i, n-1, grid);
+            }
         }
-        for(int j=0; j<n; j++)
+        for(int i=0; i<n; i++)
         {
-            if(grid[0][j] == 0) removeCorners(0, j, grid);
-            if(grid[m-1][j] == 0) removeCorners(m-1, j, grid);
+            if(grid[0][i]==0)
+            {
+                dfs(0, i, grid);
+            }
+            if(grid[m-1][i]==0)
+            {
+                dfs(m-1, i, grid);
+            }
         }
-        int ans = 0;
-        for(int i=1; i<m-1; i++) 
+        int ans=0;
+        for(int i=1; i<m-1; i++)
         {
             for(int j=1; j<n-1; j++)
             {
-                if(grid[i][j] == 0) 
+                if(grid[i][j]==0)
                 {
                     ans++;
-                    closed(i, j, grid);
+                    dfs(i, j, grid);
                 }
             }
         }
