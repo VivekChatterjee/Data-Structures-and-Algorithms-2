ea6f5cc32ef9bd4a57e01a6610d5b97721068a3f
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-04-15 16:50:19 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find Total Time Taken/find-total-time-taken.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int totalTime(int n, vector<int> &arr, vector<int> &time) {
+        // code here
+        unordered_map<int, int>m;
+        int ans=0;
+        m[arr[0]]++;
+        for(int i=1; i<n; i++)
+        {
+            if(m.count(arr[i]))
+            {
+                ans+=time[arr[i]-1];
+            }
+            else
+            {
+                ans++;
+            }
+            m[arr[i]]++;
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        
+        vector<int> time(n);
+        Array::input(time,n);
+        
+        Solution obj;
+        int res = obj.totalTime(n, arr, time);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
