dc0780c63db86f6f3dbb60a06120947c57667dd4
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-04-22 12:17:02 +0530

    Time: 97 ms (34.63%), Space: 28.5 MB (50.59%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    int lcs(int i, int j, string &a, string &b, vector<vector<int>>&dp)
+    int ins(int i, int j, string &s, vector<vector<int>>&dp)
     {
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(a[i]==b[j]) return dp[i][j]=1+lcs(i-1,j-1,a,b,dp);
-        else return dp[i][j]=max(lcs(i-1, j, a, b, dp), lcs(i, j-1, a, b, dp));
-    }
-    int minInsertions(string s) {
         int n=s.size();
-        int ans=0;
-        int i=0, j=n-1;
-        string st=s;
-        reverse(st.begin(), st.end());
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int take=1e8, takeLeft=1e8, takeRight=1e8;
+        if(s[i]==s[j])
+        {
+            take=0+ins(i+1, j-1, s, dp);
+        }
+        else
+        {
+            takeLeft=1+ins(i+1, j, s, dp);
+            takeRight=1+ins(i, j-1, s, dp);
+        }
+        return dp[i][j]=min({take, takeLeft, takeRight});
+    }
+    int minInsertions(string &s) {
+        int n=s.size();
         vector<vector<int>>dp(n, vector<int>(n, -1));
-        return n-lcs(n-1, n-1, s, st, dp);
+        return ins(0, n-1, s, dp);
     }
 };
