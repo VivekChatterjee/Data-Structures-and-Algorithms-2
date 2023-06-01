6e91b879a8399e0efc75a62fd45249a103c70df5
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-06-01 11:12:02 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Topological sort/topological-sort.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+	public:
+	void dfs(int node, vector<int>adj[], vector<int>&vis, stack<int>&s)
+	{
+	    vis[node]=1;
+	    for(auto it: adj[node])
+	    {
+	        if(!vis[it]) dfs(it, adj, vis, s);
+	    }
+	    s.push(node);
+	}
+	vector<int> topoSort(int v, vector<int> adj[]) 
+	{
+	    stack<int>s;
+	    vector<int>vis(v);
+	    for(int i=0; i<v; i++)
+	    {
+	        if(!vis[i]) dfs(i, adj, vis, s);
+	    }
+	    vector<int>ans;
+	    while(!s.empty())
+	    {
+	        ans.push_back(s.top());
+	        s.pop();
+	    }
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    
+    if(V!=res.size())
+    return 0;
+    
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        
+        Solution obj;
+        vector <int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
