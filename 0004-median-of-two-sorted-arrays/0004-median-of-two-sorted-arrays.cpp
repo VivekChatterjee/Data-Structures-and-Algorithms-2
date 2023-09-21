428fd51103d02c186f3d7c606d32d7de0d6a61ec
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-09-21 21:59:03 +0530

    Time: 23 ms (76.84%), Space: 89.5 MB (55.15%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,37 @@
+// binary search is awesome, power of bs is shown in this question
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size();
+        int n2=nums2.size();
+        if(n2<n1) return findMedianSortedArrays(nums2,nums1);
+        int s=0,e=n1;
+        while(s<=e)
+        {
+            int cut1=(s+e)/2;
+            int cut2=(n1+n2+1)/2 - cut1;
+            
+            int l1= (cut1==0)? INT_MIN : nums1[cut1-1];
+            int l2= (cut2==0)? INT_MIN : nums2[cut2-1];
+            
+            int r1=(cut1==n1)? INT_MAX : nums1[cut1];
+            int r2=(cut2==n2)? INT_MAX : nums2[cut2];
+            
+            if(l1<=r2 && l2<=r1)
+            {
+                if((n1+n2) %2 ==0 )
+                    return ((max(l1,l2) + min(r1,r2))/2.0);
+                
+                else
+                    return max(l1,l2);
+            }
+            
+            else if(l1>r2)
+                e=cut1-1;
+            
+            else
+                s=cut1+1;
+        }
+        return 0.0;
+    }
+};
