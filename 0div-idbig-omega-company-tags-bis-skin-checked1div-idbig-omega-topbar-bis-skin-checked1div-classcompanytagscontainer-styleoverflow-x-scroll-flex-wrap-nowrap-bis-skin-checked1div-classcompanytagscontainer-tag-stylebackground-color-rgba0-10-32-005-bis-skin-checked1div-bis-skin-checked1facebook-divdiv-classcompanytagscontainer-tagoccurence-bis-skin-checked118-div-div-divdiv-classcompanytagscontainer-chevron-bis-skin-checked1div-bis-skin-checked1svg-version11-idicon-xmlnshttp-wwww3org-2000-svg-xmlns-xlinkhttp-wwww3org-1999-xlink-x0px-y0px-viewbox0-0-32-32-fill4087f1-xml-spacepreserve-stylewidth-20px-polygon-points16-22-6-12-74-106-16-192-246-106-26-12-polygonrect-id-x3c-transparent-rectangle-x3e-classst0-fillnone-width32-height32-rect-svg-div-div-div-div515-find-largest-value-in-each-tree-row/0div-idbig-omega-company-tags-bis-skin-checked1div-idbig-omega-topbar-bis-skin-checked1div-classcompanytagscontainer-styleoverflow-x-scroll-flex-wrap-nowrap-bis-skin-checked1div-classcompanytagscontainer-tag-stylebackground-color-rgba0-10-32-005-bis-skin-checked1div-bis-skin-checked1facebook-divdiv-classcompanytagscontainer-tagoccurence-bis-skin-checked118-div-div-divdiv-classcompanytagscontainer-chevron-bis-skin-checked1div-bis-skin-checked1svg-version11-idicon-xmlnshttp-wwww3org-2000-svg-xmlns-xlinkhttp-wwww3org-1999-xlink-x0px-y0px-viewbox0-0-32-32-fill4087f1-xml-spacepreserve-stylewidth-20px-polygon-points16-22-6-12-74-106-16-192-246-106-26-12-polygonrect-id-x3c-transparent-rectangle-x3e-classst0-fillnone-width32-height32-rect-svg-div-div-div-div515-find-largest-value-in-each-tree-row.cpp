858c69f111b533185e8b858f68edd39348775d1f
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2023-10-24 19:17:01 +0530

    Time: 7 ms (81.90%), Space: 22.6 MB (32.41%) - LeetHub

---
+++ 0div-idbig-omega-company-tags-bis-skin-checked1div-idbig-omega-topbar-bis-skin-checked1div-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-bis-skin-checked1div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-bis-skin-checked1div-bis-skin-checked1facebook-divdiv-classcompanytagscontainer-tagoccurence-bis-skin-checked118-div-div-divdiv-classcompanytagscontainer-chevron-bis-skin-checked1div-bis-skin-checked1svg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div515-find-largest-value-in-each-tree-row/0div-idbig-omega-company-tags-bis-skin-checked1div-idbig-omega-topbar-bis-skin-checked1div-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-bis-skin-checked1div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-bis-skin-checked1div-bis-skin-checked1facebook-divdiv-classcompanytagscontainer-tagoccurence-bis-skin-checked118-div-div-divdiv-classcompanytagscontainer-chevron-bis-skin-checked1div-bis-skin-checked1svg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div515-find-largest-value-in-each-tree-row.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        if(!root) return {};
+        queue<TreeNode*>q;
+        q.push(root);
+        vector<int>ans;
+        while(!q.empty())
+        {
+            int n=q.size();
+            int maxi=q.front()->val;
+            while(n--)
+            {
+                TreeNode* cur = q.front();
+                q.pop();
+                if(cur->left) q.push(cur->left);
+                if(cur->right) q.push(cur->right);
+                maxi=max(maxi, cur->val);
+            }
+            ans.push_back(maxi);
+        }
+        return ans;
+    }
+};
