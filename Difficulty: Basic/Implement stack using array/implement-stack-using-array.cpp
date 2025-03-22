c4a5b24d7c0b91ab9ea44c8fa167dab1ca1a8af9
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2025-03-22 10:02:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Implement stack using array/implement-stack-using-array.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyStack {
+  private:
+    int arr[1000];
+    int top;
+
+  public:
+    MyStack() { top = -1; }
+
+    int pop();
+    void push(int);
+};
+
+
+// } Driver Code Ends
+
+// Function to push an integer into the stack.
+
+/*
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+*/
+
+void MyStack ::push(int x) {
+    // Your Code
+    if(top==-1){
+        top=0;
+        arr[top]=x;
+    }
+    else{
+        arr[top+1]=x;
+        top++;
+    }
+}
+
+// Function to remove an item from top of the stack.
+int MyStack ::pop() {
+    if(top==-1) return top;
+    // Your Code
+    int cur=arr[top];
+    arr[top]=0;
+    top--;
+    return cur;
+}
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore(); // Flush the newline character left by cin
+    while (T--) {
+        MyStack *sq = new MyStack();
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        int n = nums.size();
+        int i = 0;
+        while (i < n) {
+            int QueryType = nums[i++];
+            if (QueryType == 1) {
+                int a = nums[i++];
+                sq->push(a);
+            } else if (QueryType == 2) {
+                cout << sq->pop() << " ";
+            }
+        }
+        cout << "\n~\n";
+        delete sq;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
